b11fe737b6a8476cb032b210ddccba5e95af4975
Jan Hrček <honza.hrk@gmail.com>
2025-05-26 18:09:07 +0200

    More haddocks, link to openai docs

--- src/OpenAI/V1/Audio/Speech.hs
+++ src/OpenAI/V1/Audio/Speech.hs
@@ -1,4 +1,4 @@
--- | @\/v1\/audio\/speech@
+-- | [@\/v1\/audio\/speech@](https://platform.openai.com/docs/api-reference/audio/createSpeech)
 module OpenAI.V1.Audio.Speech
     ( -- * Main types
       CreateSpeech(..)
@@ -27,7 +27,7 @@ instance FromJSON Voice where
 instance ToJSON Voice where
     toJSON = genericToJSON aesonOptions
 
--- | The format to audio in
+-- | The format to generate the audio in
 data Format = MP3 | Opus | AAC | FLAC | WAV | PCM
     deriving stock (Bounded, Enum, Generic, Show)
 
@@ -40,11 +40,13 @@ instance ToJSON Format where
 -- | Request body for @\/v1\/audio\/speech@
 data CreateSpeech = CreateSpeech
     { model :: Model -- ^ Note that only [TTS models](https://platform.openai.com/docs/models#tts) support speech synthesis.
-    , input :: Text
+    , input :: Text -- ^ The text to generate audio for. The maximum length is 4096 characters.
     , voice :: Voice
     , instructions :: Maybe Text -- ^ Instructions for the model to follow when generating the audio.
-    , response_format :: Maybe Format -- ^ Uses 'MP3' if not specified.
+                                 -- Does not work with @tts-1@ or @tts-1-hd@ models.
+    , response_format :: Maybe Format -- ^ Defaults to 'MP3'.
     , speed :: Maybe Double -- ^ Defaults to 1.0 (normal speed). Should be between 0.25 and 4.0.
+                            -- Does not work with @gpt-4o-mini-tts@ model.
     } deriving stock (Generic, Show)
 
 instance ToJSON CreateSpeech where
